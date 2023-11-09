daead178f125d6372547dc8ef7c8b0c3b67adb6d
Matheus Pavanelli <mspavanelli@gmail.com>
2023-11-09 01:02:10 -0300

    feat: add param name validation

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import { useRouter } from "next/router";
 
 export default function Home() {
   const { query } = useRouter();
+  const person = Boolean(query.name) ? query.name : "Anonymous";
 
-  return <div style={{ fontFamily: "sans-serif" }}>Hello, {query.name}!</div>;
+  return <div style={{ fontFamily: "sans-serif" }}>Hello, {person}!</div>;
 }
